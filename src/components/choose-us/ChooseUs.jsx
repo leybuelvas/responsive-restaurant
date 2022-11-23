2e88247d470e5da3a68b3ae3027e816af50579af
leybuelvas <leycibuelvas73@gmail.com>
2022-11-23 21:27:18 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/choose-us/ChooseUs.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { Container, Row, Col } from "reactstrap";
+import "./choose-us.css";
+import pastaImg from "../../assets/images/pasta.png";
+
+const ChooseUs = () => {
+  return (
+    <section>
+        <Container>
+            <Row>
+                <Col lg='6' md='6'>
+                    <img src={pastaImg} alt="" className='w-100' />
+                </Col>
+
+                <Col lg='6' md='6'>
+                    <div className="choose__content">
+                        <h4>Who we are?</h4>
+                        <h2>Take a look at the benefits we offer you</h2>
+                        <p>
+                            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
+                    </div>
+
+                    <div className="features mt-4">
+                        <div className="feature1 d-flex align-items-center gap-5">
+                            <div className="single__feature">
+                                <span>
+                                    <i class="ri-truck-line"></i>
+                                </span>
+                                <h6>Free Home Delivery</h6>
+                                <p>Lorem ipsum dolor sit amet.</p>
+                            </div>
+
+                            <div className="single__feature">
+                                <span className="feature__icon-two">
+                                    <i class="ri-money-dollar-circle-line"></i>
+                                </span>
+                                <h6>Return & Refund</h6>
+                                <p>Lorem ipsum dolor sit amet.</p>
+                            </div>
+                        </div>
+
+                        <div className="feature1 mt-3 d-flex align-items-center gap-5">
+                            <div className="single__feature">
+                                <span className="features__icon-3">
+                                    <i class="ri-secure-payment-line"></i>
+                                </span>
+                                <h6>Secure Payment</h6>
+                                <p>Lorem ipsum dolor sit amet.</p>
+                            </div>
+
+                            <div className="single__feature">
+                                <span className="feature__icon-4">
+                                    <i class="ri-24-hours-line"></i>
+                                </span>
+                                <h6>24/7 Hours Support</h6>
+                                <p>Lorem ipsum dolor sit amet.</p>
+                            </div>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    </section>
+  );
+};
+
+export default ChooseUs
